85c4797a6f420a2ac9cfa0dfa34be234e269628c
fada.yu <fada.yu@zhaopin.com.cn>
2021-08-21 15:51:04 +0800

    周末提交 订单宽表初始化

---
+++ gmall2021-realtime/src/main/java/com/alibaba/bean/OrderDetail.java
@@ -0,0 +1,27 @@
+package com.alibaba.bean;
+
+import lombok.Data;
+
+import java.math.BigDecimal;
+
+/**
+ * @author fada.yu
+ * @date 2021/8/21 15:30
+ * @Description：
+ * 订单明细实体类
+ */
+@Data
+public class OrderDetail {
+    Long id;
+    Long order_id ;
+    Long sku_id;
+    BigDecimal order_price ;
+    Long sku_num ;
+    String sku_name;
+    String create_time;
+    BigDecimal split_total_amount;
+    BigDecimal split_activity_amount;
+    BigDecimal split_coupon_amount;
+    Long create_ts;
+
+}
